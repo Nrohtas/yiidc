481ab230bdb1a822c28b3857ed45b3d611fdcfd7
tehnn <tehnnn@gmail.com>
2016-07-28 14:07:44 +0700

    utehn-new-init

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yiidb;port=3309',
+            'dsn' => 'mysql:host=localhost;dbname=yiidc;port=3309',
             'username' => 'root',
             'password' => '1234',
             'charset' => 'utf8',
